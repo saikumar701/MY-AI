9eb7875c35fa5913b118ad6130433c1b01b57326
sumu9897 <sumon.bncd@gmail.com>
2025-07-27 16:36:07 +0600

    Remove Background page

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,11 +1,11 @@
-import { Eraser, Sparkles } from 'lucide-react';
-import React, { useState } from 'react'
+import { Eraser, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const RemoveBackground = () => {
   const [input, setInput] = useState("");
-    const onSubmitHandler = async (e) => {
-      e.preventDefault();
-    };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/* Left col */}
@@ -21,12 +21,15 @@ const RemoveBackground = () => {
 
         <input
           onChange={(e) => setInput(e.target.files[0])}
-          type="file" accept='image/*'
+          type="file"
+          accept="image/*"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
           required
         />
-       
-       <p className='text-xs text-gray-500 font-light mt-1'>Support JPG,PNG, and other image formates</p>
+
+        <p className="text-xs text-gray-500 font-light mt-1">
+          Support JPG,PNG, and other image formates
+        </p>
         <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#f6ab41] to-[#ff4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
           <Eraser className="w-5 " /> Remove background
         </button>
@@ -45,7 +48,7 @@ const RemoveBackground = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RemoveBackground
+export default RemoveBackground;
