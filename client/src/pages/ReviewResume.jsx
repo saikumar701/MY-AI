1919dda43846adcb19a1fe43e165daaa75ed73b6
sumu9897 <sumon.bncd@gmail.com>
2025-07-27 16:47:19 +0600

    update

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -38,7 +38,7 @@ const ReviewResume = () => {
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
         <div className="flex items-center gap-3">
           <FileText className="w-5 h-5 text-[#00da83]" />
-          <h1 className="text-xl font-semibold">Processed Image</h1>
+          <h1 className="text-xl font-semibold">Analysis Result</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
