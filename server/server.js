2e81189b4c4e76be071cc95e092d3b0368b73467
sumu9897 <sumon.bncd@gmail.com>
2025-07-31 22:12:58 +0600

    user router and setup

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const app = express()
+
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
@@ -18,12 +22,11 @@ app.get('/', (req, res)=>res.send('Server is Live!'))
 app.use(requireAuth())
 
 app.use('/api/ai', aiRouter)
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log('Server is running in port', PORT);
-    console.log("CLERK_PUBLISHABLE_KEY:", process.env.CLERK_PUBLISHABLE_KEY);
-
     
 })
