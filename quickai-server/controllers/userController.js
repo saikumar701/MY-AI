c10afe5089c88a9bac9e265b3c738eaed3fc7037
sumu9897 <sumon.bncd@gmail.com>
2025-08-01 10:26:23 +0600

    Project Completed

--- quickai-server/controllers/userController.js
+++ quickai-server/controllers/userController.js
@@ -50,7 +50,7 @@ export const getPublishedCreations = async (req, res) => {
             message = 'Creation Liked'
         }
 
-        const formattedArray = `{${updatedLikes.json(',')}}`
+        const formattedArray = `{${updatedLikes.join(',')}}`
 
         await sql `UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${id}`
       res.json({ success: true, message });
