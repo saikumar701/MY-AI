b741a46550350446bde7d6ee72edbcb2581f6846
SaiKumar701 <saikumar033333@gmail.com>
2025-08-14 20:19:53 +0530

    pushing project files

--- quickai-client/src/pages/Dashboard.jsx
+++ quickai-client/src/pages/Dashboard.jsx
@@ -8,6 +8,7 @@ import toast from 'react-hot-toast'
 
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.withCredentials = true;
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([])
--- quickai-server/server.js
+++ quickai-server/server.js
@@ -11,7 +11,10 @@ const app = express()
 
 await connectCloudinary()
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
 app.use(clerkMiddleware())
 
